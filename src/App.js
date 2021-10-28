852df75906ae56b5dcb7226de1ec3a37cacc0b96
sophiecodescodes <sophiecodes02@gmail.com>
2021-10-28 16:32:43 +0800

    Update App.js
    removed one of the "Learn Snail" links

--- src/App.js
+++ src/App.js
@@ -18,14 +18,6 @@ function App() {
         >
           Learn Snail
         </a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Snail
-        </a>
       </header>
       <img id="smol" src="https://i.pinimg.com/originals/a6/06/21/a60621da98a79bfbdc0e3dd5524a32d3.gif" alt="smol snel"></img>
     </div>
