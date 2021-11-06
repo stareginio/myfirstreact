73b91bfd76121e9c5c9509ce348ee8c58b3ffe72
stareginio <sophiecodes02@gmail.com>
2021-11-06 17:27:28 +0800

    Update Hello.js
    added useEffect

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import helloServices from '../services/helloServices';
 
 const Hello = () => {
     // react hooks
     const [hello, setHello] = useState("");
+
+    return helloServices.getHello();
+
+    // react hooks useEffect
+    // axios - tool for http request promises
+    // promises - asynch processes
+    useEffect(() => {
+        helloServices.getHello()
+            .then(response => {
+                setHello(response.data)
+            })
+            .catch(err => {
+                console.log("Something went wrong...")
+            })
+    }
+    )
 
     return helloServices.getHello();
 }
