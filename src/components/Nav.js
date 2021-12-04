ae2146d5dd66d6bcc7ac97d6a620177a1bdfedfd
stareginio <sophiecodes02@gmail.com>
2021-12-04 15:43:26 +0800

    Update Nav.css, Nav.js
    modified the css file, added a toggler button for the nav links in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,15 @@
+import '../styles/Nav.css';
+
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
+                <a class="navbar-brand" href="#">Snail</a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <div className="navbar-nav" id="navbarNavAltMarkup">
                         <a className="nav-link" aria-current="page" href="/">Home</a>
