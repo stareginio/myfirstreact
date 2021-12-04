b9a5b945ef4eabc0358051fc82dab233d646e30e
stareginio <sophiecodes02@gmail.com>
2021-12-04 16:16:55 +0800

    Add Home.js, Home.css
    added Home.js, renamed App.css into Home.css, modified App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import logo from './logo.gif';
-import './styles/App.css';
-
-import Button from './components/Button';
-import Employee from './components/Employee';
-import Greet from './components/Greet';
-import Hello from './components/Hello';
+import Home from './components/Home';
 import Nav from './components/Nav';
 
 // functional component = stateless component
@@ -12,48 +6,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <h1> s n a i l </h1>
-
-        <p id="hello"> {Hello()} </p>
-
-        <Button />
-
-        <hr />
-
-        <div id="greet">
-          <Greet name="Snail Sr." />
-          <Greet name="Snail Jr." />
-        </div>
-
-        <hr />
-
-        <Employee />
-
-        <hr />
-
-        <img src="https://i.gifer.com/7dTK.gif"
-          className="App-gif"
-          alt="snel gif">
-        </img>
-
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=Tt7bzxurJ1I"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          here's a cute dancing stick bug video
-        </a>
-
-      </header>
-      <img id="smol"
-        src="https://i.pinimg.com/originals/a6/06/21/a60621da98a79bfbdc0e3dd5524a32d3.gif"
-        alt="smol snel">
-      </img>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,61 @@
+import logo from './logo.gif';
+import '../styles/Home.css';
+
+import Button from './Button';
+import Employee from './Employee';
+import Greet from './Greet';
+import Hello from './Hello';
+import Nav from './Nav';
+
+// functional component = stateless component
+function Home() {
+    return (
+        <div className="Home">
+            <Nav />
+            <header className="Home-header">
+
+                <img src={logo} className="Home-logo" alt="logo" />
+
+                <h1> s n a i l </h1>
+
+                <p id="hello"> {Hello()} </p>
+
+                <Button />
+
+                <hr />
+
+                <div id="greet">
+                    <Greet name="Snail Sr." />
+                    <Greet name="Snail Jr." />
+                </div>
+
+                <hr />
+
+                <Employee />
+
+                <hr />
+
+                <img src="https://i.gifer.com/7dTK.gif"
+                    className="Home-gif"
+                    alt="snel gif">
+                </img>
+
+                <a
+                    className="Home-link"
+                    href="https://www.youtube.com/watch?v=Tt7bzxurJ1I"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    here's a cute dancing stick bug video
+                </a>
+
+            </header>
+            <img id="smol"
+                src="https://i.pinimg.com/originals/a6/06/21/a60621da98a79bfbdc0e3dd5524a32d3.gif"
+                alt="smol snel">
+            </img>
+        </div>
+    );
+}
+
+export default Home;
