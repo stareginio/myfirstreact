e9a6bb7a2cbc92fbf0ace1ba34aa4dad4b6a02b7
stareginio <sophiecodes02@gmail.com>
2021-12-15 07:43:03 +0800

    Update httpclient.js

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://myfirstfullstack.herokuapp.com",
+    baseURL: "https://myfirstfullstack.herokuapp.com/api/v1/",
     headers: {
         'Content-Type': "application/json"
     }
