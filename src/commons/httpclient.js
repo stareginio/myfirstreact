44a005c13dc348c8a2f56bad3052db838100b0b2
stareginio <sophiecodes02@gmail.com>
2021-12-15 07:21:19 +0800

    Revert "Update httpclient.js"
    This reverts commit ee3cbb1eef4d8d16084841c25175917246a861e8.

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://myfirstfullstack.herokuapp.com",
+    baseURL: "https://myfirstfullstack.herokuapp.com/api/v1/employees",
     headers: {
         'Content-Type': "application/json"
     }
